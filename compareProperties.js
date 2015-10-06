5a299b06dee3da26d2a9b929f090ead4868b50a2
hundekoerper <el.danowar@googlemail.com>
2015-10-06 14:43:02 +0200

    implement used-at-line output

--- compareProperties.js
+++ compareProperties.js
@@ -3,7 +3,8 @@ var comepareProperties = function(usedproperties, database, profile) {
   var chalk = require('chalk')
   ,   _ = require('underscore')
   ,   browser = []
-  ,   matches = [];
+  ,   matches = []
+  ,   temp = [];
 
   for (var bsr in profile.browsers) {
     if (profile.browsers[bsr] !== 0) {
@@ -17,11 +18,24 @@ var comepareProperties = function(usedproperties, database, profile) {
     }
   };
 
-// for (var i = 0, x = matches.length; i < x; i ++) {
-//   if
-// }
-//
-//   matches = _.union(matches[0], matches[1]);
+for (var i = 0, x = matches.length; i < x ; i++) {
+  for (var j = i + 1; j < x; j++) {
+    if (matches[i][0] === matches[j][0]) {
+      matches[i] = _.union(matches[i], matches[j]);
+      temp.push(j);
+    }
+  }
+}
+
+temp = _.uniq(temp);
+
+for (var i = 0, x = temp.length; i < x; i++) {
+  delete matches[temp[i]];
+}
+
+matches = _.compact(matches);
+temp = [];
+
 
   if (matches.length === 0){
     console.log("==================");
@@ -34,13 +48,22 @@ var comepareProperties = function(usedproperties, database, profile) {
     console.log("==================");
     console.log("");
   } else {
-    // for(var i = 0, x = matches.length; i < x; i++) {
-    //   console.log("==================");
-    //   console.log("You used " + chalk.bold(matches[i][0]));
-    //   console.log("at Line " + matches[i][1]);
-    //   console.log("==================");
-    // }
-    console.log(matches);
+    for(var i = 0, x = matches.length; i < x; i++) {
+      console.log("==================");
+      console.log("You used " + chalk.bold(matches[i][0]) + " at");
+      for(var j = 1, y = matches[i].length; j < y; j++) {
+        temp.push(matches[i][j]);
+      }
+      console.log("Line: " + temp);
+      console.log("==================");
+      temp = [];
+      if (profile.description === 1) {
+      console.log(database.data[matches[i][0]].title);
+      console.log("");
+      console.log(database.data[matches[i][0]].description);
+      console.log("");
+      }
+    }
   }
 
 
