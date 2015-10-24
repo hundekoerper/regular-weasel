15b42372be74e5ca00ab1a84f17c82c83d0a38c0
hundekoerper <el.danowar@googlemail.com>
2015-10-24 21:57:57 +0200

    move options to cli.js

--- cli.js
+++ cli.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 
-var weasel = require('./lib/index.js');
-weasel();
+var weasel = require('./lib/index.js')
+,   profile = require("./profile.json")
+,   argv = require("minimist")(process.argv.slice(2));
+
+if (profile.read_from_json === 0) {
+  profile.filepath = argv._[0];
+}
+
+weasel(profile);
+
+
