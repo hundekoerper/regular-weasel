d1684e2f3f243a5a4dffdf4add6a3711c753594c
hundekoerper <el.danowar@googlemail.com>
2015-10-24 22:38:33 +0200

    make lib even more stupid

--- cli.js
+++ cli.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 
 var weasel = require('./lib/index.js')
+,   fs = require("fs")
 ,   profile = require("./profile.json")
-,   argv = require("minimist")(process.argv.slice(2));
+,   argv = require("minimist")(process.argv.slice(2))
+,   chalk = require("chalk");
 
 if (profile.read_from_json === 0) {
   profile.filepath = argv._[0];
 }
 
-weasel(profile);
+fs.readFile(profile.filepath, "utf-8", function(err, contents){
+  if (err) {
+    console.error(chalk.red('ERROR: ' + err.message));
+    process.exit(1);
+  }
 
+  delete profile.filepath;
 
+  weasel(profile, contents);
+});
