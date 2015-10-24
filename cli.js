5a4f08d2cd1894bebc89aa26ff2a90459c469259
hundekoerper <el.danowar@googlemail.com>
2015-10-24 22:59:19 +0200

    throw error when no browser is set in profile.json

--- cli.js
+++ cli.js
@@ -10,13 +10,21 @@ if (profile.read_from_json === 0) {
   profile.filepath = argv._[0];
 }
 
+function exit(err) {
+  console.error(chalk.red(err.stack));
+  process.exit(1);
+}
+
 fs.readFile(profile.filepath, "utf-8", function(err, contents){
   if (err) {
-    console.error(chalk.red('ERROR: ' + err.message));
-    process.exit(1);
+    exit(err);
   }
 
   delete profile.filepath;
 
-  weasel(profile, contents);
+  try {
+    weasel(profile, contents);
+  } catch (e) {
+    exit(e);
+  }
 });
